da30302fecfabc83355947a34ad4a595627d98df
Aleksandr <gorokhovao@gmail.com>
2022-02-09 22:49:51 +0900

    Initial commit

---
+++ src/main/java/org/group_gb/tg_bot/TgBotApplication.java
@@ -0,0 +1,14 @@
+package org.group_gb.tg_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgBotApplication {
+
+	public static void main(String[] args) {
+
+        SpringApplication.run(TgBotApplication.class, args);
+	}
+
+}
